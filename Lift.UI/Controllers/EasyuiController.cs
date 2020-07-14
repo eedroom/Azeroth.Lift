7a20489f3a7ae0fc9b5442d418416ec94648fdb2
邓常青 <eedroom@163.com>
2020-07-15 00:30:58 +0800

    整合easyui的demo

--- Lift.UI/Controllers/EasyuiController.cs
+++ Lift.UI/Controllers/EasyuiController.cs
@@ -18,5 +18,28 @@ namespace Lift.UI.Controllers
         {
             return View();
         }
+
+        public ActionResult Treedata()
+        {
+            var path= this.HttpContext.Server.MapPath("~/Assets/jquery-easyui-1.5.2/demo");
+            var lstTreedata= new System.IO.DirectoryInfo(path)
+                .GetDirectories()
+                .Select(x => new
+                {
+                    id = x.Name,
+                    text = x.Name,
+                    state = "closed",
+                    children = x.GetFiles().Select(a => new
+                    {
+                        id = x.Name + "-" + a.Name,
+                        text = a.Name,
+                        attributes = new
+                        {
+                            url=this.Url.Content(string.Format("~/Assets/jquery-easyui-1.5.2/demo/{0}/{1}",x.Name,a.Name))
+                        }
+                    }).ToList()
+                }).ToList();
+            return this.Json(lstTreedata, JsonRequestBehavior.AllowGet);
+        }
     }
 }
