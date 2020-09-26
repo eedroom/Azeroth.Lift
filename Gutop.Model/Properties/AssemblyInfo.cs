9f8a8070e89becd91af190db90fdfe33b9ef618f
邓常青 <eedroom@163.com>
2020-09-26 21:57:31 +0800

    优化命名

--- Gutop.Model/Properties/AssemblyInfo.cs
+++ Gutop.Model/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Lift.Model")]
+[assembly: AssemblyTitle("Gutop.Model")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lift.Model")]
+[assembly: AssemblyProduct("Gutop.Model")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
