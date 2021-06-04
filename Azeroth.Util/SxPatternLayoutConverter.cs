a7d7106756ff4b22f791cde7e2b414f22bdd51ad
邓常青 <eedroom@163.com>
2021-06-04 23:43:03 +0800

    log4net的使用优化

--- Azeroth.Util/SxPatternLayoutConverter.cs
+++ Azeroth.Util/SxPatternLayoutConverter.cs
@@ -8,20 +8,22 @@ using log4net.Core;
 
 namespace Azeroth.Util {
     public class SxPatternLayoutConverter : log4net.Layout.Pattern.PatternLayoutConverter {
+        static System.Reflection.Assembly fwAsm = typeof(string).Assembly;
         protected override void Convert(TextWriter writer, LoggingEvent loggingEvent) {
-            var pp= loggingEvent.MessageObject.GetType().GetProperty(this.Option, System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public);
-            if (pp == null) {
-                writer.Write(string.Format("指定的属性[{0}]未找到", this.Option));
+            var meta = loggingEvent.MessageObject.GetType();
+            if (meta.Assembly == fwAsm)
                 return;
-            }
-            var value= pp.GetValue(loggingEvent.MessageObject);
-            var valuestr= Newtonsoft.Json.JsonConvert.SerializeObject(value);
-            if (!valuestr.StartsWith("\"")) {
-                writer.Write(valuestr);
+            var sxMeta= meta.GetProperty(this.Option, System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public);
+            if (sxMeta == null)
                 return;
+            var value= sxMeta.GetValue(loggingEvent.MessageObject);
+            if (value == null)
+                return;
+            if (sxMeta.PropertyType.Assembly == fwAsm) {
+                writer.Write(value.ToString());
             }
-            var tmp = valuestr.Substring(1, valuestr.Length - 2);
-            writer.Write(tmp);
+            var valuejson= Newtonsoft.Json.JsonConvert.SerializeObject(value);
+            writer.Write(valuejson);
         }
     }
 }
